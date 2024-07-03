1fa5ac28d515fe7cbe6970bb35fb62aee2db33e6
Yash shinde <yashshinde.tech@gmail.com>
2024-07-03 08:54:56 +0530

    file laptop

---
+++ Laptop.java
@@ -0,0 +1,13 @@
+package com.example.App;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop { 
+	 
+		public void complile() {
+			System.out.println("laptop is working");
+		}
+	}
+
+
